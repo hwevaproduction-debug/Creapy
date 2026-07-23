ca0e60447e3fc12201805c77ef165b04299649e1
donnie-alvin <hwevaproduction@gmail.com>
2026-07-23 03:50:42 +0200

    0318

--- real-app-backend-main/controllers/uploadController.js
+++ real-app-backend-main/controllers/uploadController.js
@@ -5,6 +5,7 @@ const AppError = require('../utils/appError');
 
 const s3ClientConfig = {
   region: process.env.S3_REGION || process.env.AWS_REGION,
+  requestChecksumCalculation: 'WHEN_REQUIRED',
 };
 
 if (process.env.S3_ACCESS_KEY_ID && process.env.S3_SECRET_ACCESS_KEY) {
